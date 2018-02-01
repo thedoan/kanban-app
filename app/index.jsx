dd7673447c732d4d02588d25fe271a2e0ad206fe
The Doan <dangthedoan@gmail.com>
2018-02-01 19:20:11 +0700

    first commit

---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+//Test HMR
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ app/index_.js
@@ -0,0 +1,5 @@
+require('./main.css');
+var component = require('./component');
+var app = document.createElement('div');
+document.body.appendChild(app);
+app.appendChild(component());
